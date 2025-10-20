69a631a4e5872504c704004e1d6186e805f8b25e
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-10-20 09:26:19 +0530

    stateful component + grid with delete

--- src/components/ExpenseEntryItemList4.js
+++ src/components/ExpenseEntryItemList4.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './ExpenseEntryItemList.css';
+import './ExpenseEntryItemList3.css';
 
-class ExpenseEntryItemList3 extends React.Component {
+class ExpenseEntryItemList4 extends React.Component {
     constructor(props) {
         super(props);
-
-
+        this.state = {
+            items: this.props.items
+        }
         this.handleMouseEnter = this.handleMouseEnter.bind();
         this.handleMouseLeave = this.handleMouseLeave.bind();
         this.handleMouseOver = this.handleMouseOver.bind();
@@ -19,13 +20,37 @@ class ExpenseEntryItemList3 extends React.Component {
     handleMouseOver(e) {
         console.log("The mouse is at (" + e.clientX + ", " + e.clientY + ")");
     }
+    handleDelete = (id, e) => {
+        e.preventDefault();
+        console.log(id);
+        this.setState((state, props) => {
+            let items = [];
+            state.items.forEach((item, idx) => {
+                if (item.id != id)
+                    items.push(item)
+            })
+            let newState = {
+                items: items
+            }
+            return newState;
+        })
+    }
+    getTotal() {
+        let total = 0;
+        for (var i = 0; i < this.state.items.length; i++) {
+            total += this.state.items[i].amount
+        }
+        return total;
+    }
     render() {
-        const lists = this.props.items.map((item) =>
+        const lists = this.state.items.map((item) =>
             <tr key={item.id} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
                 <td>{item.name}</td>
                 <td>{item.amount}</td>
                 <td>{new Date(item.spendDate).toDateString()}</td>
                 <td>{item.category}</td>
+                <td><a href="#"
+                    onClick={(e) => this.handleDelete(item.id, e)}>Remove</a></td>
             </tr>
         );
         return (
@@ -36,13 +61,20 @@ class ExpenseEntryItemList3 extends React.Component {
                         <th>Amount</th>
                         <th>Date</th>
                         <th>Category</th>
+                        <th>Remove</th>
                     </tr>
                 </thead>
                 <tbody>
                     {lists}
+                    <tr>
+                        <td colSpan="1" style={{ textAlign: "right" }}>Total Amount</td>
+                        <td colSpan="4" style={{ textAlign: "left" }}>
+                            {this.getTotal()}
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         );
     }
 }
-export default ExpenseEntryItemList3;
+export default ExpenseEntryItemList4;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ExpenseEntryItemList3 from './components/ExpenseEntryItemList3'
+import ExpenseEntryItemList4 from './components/ExpenseEntryItemList4'
+
 
 // import App from './components/App';
 // import App2 from './components/App2';
@@ -9,6 +10,8 @@ import ExpenseEntryItemList3 from './components/ExpenseEntryItemList3'
 // import ExpenseEntryItem from './components/ExpenseEntryItem'
 // import ExpenseEntryItemList2 from './components/ExpenseEntryItemList2';
 // import MessageWithEvent from './components/MessageWithEvent'
+// import ExpenseEntryItemList3 from './components/ExpenseEntryItemList3'
+// import Clock2 from './components/Clock2';
 
 // import ExpenseEntryItem from './components/ExpenseEntryItem';
 // import ExpenseEntryItemList from './components/ExpenseEntryItemList';
@@ -114,9 +117,6 @@ ReactDOM.render(
    document.getElementById('root')
 );
 
-*/
-
-
 const items = [
     { id: 1, name: "Pizza", amount: 80, spendDate: "2020-10-10", category: "Food" },
     { id: 2, name: "Grape Juice", amount: 30, spendDate: "2020-10-12", category: "Food" },
@@ -136,6 +136,35 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Clock2 />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+
+*/
+
+const items = [
+    { id: 1, name: "Pizza", amount: 80, spendDate: "2020-10-10", category: "Food" },
+    { id: 2, name: "Grape Juice", amount: 30, spendDate: "2020-10-12", category: "Food" },
+    { id: 3, name: "Cinema", amount: 210, spendDate: "2020-10-16", category: "Entertainment" },
+    { id: 4, name: "Java Programming book", amount: 242, spendDate: "2020-10-15", category: "Academic" },
+    { id: 5, name: "Mango Juice", amount: 35, spendDate: "2020-10-16", category: "Food" },
+    { id: 6, name: "Dress", amount: 2000, spendDate: "2020-10-25", category: "Cloth" },
+    { id: 7, name: "Tour", amount: 2555, spendDate: "2020-10-29", category: "Entertainment" },
+    { id: 8, name: "Meals", amount: 300, spendDate: "2020-10-30", category: "Food" },
+    { id: 9, name: "Mobile", amount: 3500, spendDate: "2020-11-02", category: "Gadgets" },
+    { id: 10, name: "Exam Fees", amount: 1245, spendDate: "2020-11-04", category: "Academic" }
+]
+ReactDOM.render(
+    <React.StrictMode>
+        <ExpenseEntryItemList4 items={items} />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 
 
