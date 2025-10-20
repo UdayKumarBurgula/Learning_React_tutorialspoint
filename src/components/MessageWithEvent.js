ed490a4ef426bf0a99e19109466572616e19801c
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-10-20 07:42:59 +0530

    Event-Aware Component + Passing Extra Information to Event Handler

--- src/components/MessageWithEvent.js
+++ src/components/MessageWithEvent.js
@@ -6,15 +6,32 @@ class MessageWithEvent extends React.Component {
 
       this.logEventToConsole = this.logEventToConsole.bind();
    }
-   logEventToConsole(e) {
-      console.log(e.target.innerHTML);
-   }
-   render() {
-      return (
-         <div onClick={this.logEventToConsole}>
-            <p>Hello {this.props.name}!</p>
-         </div>
-      );
-   }
+
+   //logEventToConsole(e) {
+   //   console.log(e.target.innerHTML);
+   //}
+
+    logEventToConsole(msgid, e) {
+        console.log(e.target.innerHTML);
+        console.log(msgid);
+    }
+
+   //render() {
+   //   return (
+   //      <div onClick={this.logEventToConsole}>
+   //         <p>Hello {this.props.name}!</p>
+   //      </div>
+   //   );
+   //}
+
+
+    render() {
+        return (
+            <div onClick={this.logEventToConsole.bind(this, Math.floor(Math.random() * 10))}>
+                <p>Hello {this.props.name}!</p>
+            </div>
+        );
+    }
+
 }
 export default MessageWithEvent;
